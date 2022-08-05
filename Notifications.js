d287cbfba2f95fc191bcb8e3ac4ec858eae43b69
SanicBTW <58010536+SanicBTW@users.noreply.github.com>
2022-08-05 16:21:05 +0000

    the basic stuff

---
+++ Notifications.js
@@ -0,0 +1,81 @@
+//ver 2.1
+var dismissTime = 0.0;
+var increaseTimer;
+var notifying = false;
+
+var header = document.getElementById("notificationText");
+var subHeader = document.getElementById("notificationSubText");
+var notifPanel = document.getElementById("notificationsPanel");
+var dismissProgress = document.getElementById("dismissProgress");
+
+//its just the notification code on utils but improved?
+class CustomNotification
+{
+    notify()
+    {
+        if(!notifying)
+        {
+            notifying = true;
+            header.innerText = this.mainText;
+            subHeader.innerText = (this.subText != undefined ? this.subText : ' ');
+            notifPanel.style.top = "0px";
+
+            increaseTimer = setInterval(() => {
+                dismissTime += 0.5;
+
+                dismissProgress.style.width = dismissTime + "%";
+
+                if(dismissTime == 400)
+                {
+                    this.closeNotification();
+                    this.reset();
+                    this.onFinish();
+                }
+            }, 2);
+        }
+        else
+        {
+            this.closeNotification(true);
+        }
+    }
+
+    closeNotification(overriden = false)
+    {
+        if(overriden)
+        {
+            dismissTime = 0.0;
+            dismissProgress.style.width = "0%";
+            notifPanel.style.top = "-200px";
+            dismissProgress.classList.add('notransition');
+            dismissProgress.offsetHeight;
+            dismissProgress.classList.remove('notransition');
+            notifying = false;
+            clearInterval(increaseTimer);
+            setTimeout(() => {
+                var newNotif = new CustomNotification();
+                newNotif.mainText = this.mainText;
+                newNotif.subText = this.subText;
+                newNotif.onFinish = this.onFinish;
+                newNotif.notify();
+            }, 200);
+        }
+        dismissTime = 0.0;
+        dismissProgress.style.width = "0%";
+        notifPanel.style.top = "-200px";
+        notifying = false;
+        clearInterval(increaseTimer);
+    }
+
+    onFinish = function(){
+        console.log('Finished Notifying');
+    }
+
+    reset()
+    {
+        this.mainText = '';
+        this.subText = '';
+    }
+
+    mainText
+    subText
+}
---
+++ index.js
@@ -0,0 +1,30 @@
+const detectDeviceType = () => /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
+? 'Mobile'
+: 'Desktop';
+
+//we set up the "outer" class style following the device type
+function onLoad()
+{
+    var outerStyle = document.createElement("style");
+    if(detectDeviceType() == "Desktop")
+    {
+        outerStyle.innerHTML = `.outer { 
+            width: 100%; height: 97.9vh; display: flex; justify-content: center; align-items: center;
+        }`;
+    }
+    else
+    {
+        outerStyle.innerHTML = `.outer { 
+            width: 100%; height: 90vh; display: flex; justify-content: center; align-items: center;
+        }`;
+    }
+    document.head.appendChild(outerStyle);
+}
+
+function sendNotif()
+{
+    var notif = new CustomNotification();
+    notif.mainText = document.getElementById('mainNotText').value;
+    notif.subText = document.getElementById('subNotText').value;
+    notif.notify();
+}
